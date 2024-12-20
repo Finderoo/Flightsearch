b2ea0b1b2afb90c8d11087fc047feb8f17ee8310
FINDEROO <dabouloukme@gmail.com>
2024-12-20 22:57:41 +1100

    Create server.js

---
+++ server.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const { Configuration, OpenAIApi } = require('openai');
+const axios = require('axios');
